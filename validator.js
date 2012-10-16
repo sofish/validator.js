daee3a4d172be57e4d7451495f08be5c83b38467
sofish <happinesz@gmail.com>
2012-10-16 22:12:55 +0800

    尽量在每个函数调用的地方都绑定当前作用域

--- validator.js
+++ validator.js
@@ -5,7 +5,8 @@
 // 约定：以 /\$\w+/ 表示的字符，比如 $item 表示的是一个 jQuery Object
  ~function ($) {
 
-  var patterns, fields, addErrorClass, novalidate, validateForm, validateFields, radios, checkboxs, removeFromUnvalidFields, asyncValidate
+  var patterns, fields, addErrorClass, novalidate, validateForm, validateFields, radios
+    , checkboxs, removeFromUnvalidFields, asyncValidate
     , unvalidFields = []
 
   // 类型判断
@@ -57,7 +58,8 @@
       isNaN(max) && (max = text + 1);
 
       // 目前的实现 step 不能小于 0
-      return result && (isNaN(step) || 0 >= step ? (text >= min && text <= max) : 0 === (text + min) % step && (text >= min && text <= max));
+      return result && (isNaN(step) || 0 >= step ?
+        (text >= min && text <= max) : 0 === (text + min) % step && (text >= min && text <= max));
     },
 
     // 判断是否在 min / max 之间
@@ -96,7 +98,7 @@
       return this.text(text);
     },
 
-    // radio 根据当年 radio 的 name 属性获取元素，所有元素都被
+    // radio 根据当前 radio 的 name 属性获取元素，只要 name 相同的这几个元素中有一个 checked，则验证难过
     radio: function(){
       // TODO: a better way?!
       var form = this.$item.parents('form').eq(0)
@@ -114,7 +116,7 @@
     },
 
     // text[notEmpty] 表单项不为空
-    // [type=text] 也会进一项
+    // [type=text] 也会进这项
     text: function(text){
       var max = this.$item.attr('maxlength')
         , noEmpty
@@ -139,8 +141,8 @@
     params[key] = text;
 
     $[method](url, params).success(function(isValidate){
-      isValidate ? (removeErrorClass($item, klass, isErrorOnParent), false) : unvalidFields.push({
-        $el: addErrorClass($item, klass, isErrorOnParent)
+      isValidate ? (removeErrorClass.call(this, $item, klass, isErrorOnParent), false) : unvalidFields.push({
+        $el: addErrorClass.call(this, $item, klass, isErrorOnParent)
         , type: $item.attr('type') || 'text'
         , message: 'unvaild'
       })
@@ -173,14 +175,14 @@
     // 暂时去掉 radio/checkbox 的 notEmpty 检测
     if(!/^(?:radio|checkbox)$/.test(type) && !patterns['text'](val)) {
       return {
-          $el: addErrorClass($item, klass, parent)
+          $el: addErrorClass.call(this, $item, klass, parent)
         , type: type
         , error: 'empty'
       }
     }
 
     // 异步验证则不进行普通验证
-    if(async) return asyncValidate($item, val, klass, parent);
+    if(async) return asyncValidate.call(this, $item, val, klass, parent);
 
     // HTML5 pattern 支持
     // TODO: new 出来的这个正则是否与浏览器一致？
@@ -193,10 +195,10 @@
     //  , message: {String} // error message，只有两种值
     // }
     return message === 'unvalid' ? {
-        $el: addErrorClass($item, klass, parent)
+        $el: addErrorClass.call(this, $item, klass, parent)
       , type: type
       , error: 'unvalid'
-    } : (removeErrorClass($item, klass, parent), false);
+    } : (removeErrorClass.call(this, $item, klass, parent), false);
   }
 
   // 校验表单项
@@ -286,11 +288,11 @@
     novalidate($form);
 
     // 表单项校验
-    method && validateFields($items, method, klass, isErrorOnParent);
+    method && validateFields.call(this, $items, method, klass, isErrorOnParent);
 
     // 提交校验
     $form.on('submit', function(e){
-      validateForm($items, method, klass, isErrorOnParent);
+      validateForm.call(this, $items, method, klass, isErrorOnParent);
       return unvalidFields.length === 0 ? true : e.preventDefault(), errorCallback.call(this, unvalidFields);
     })
 
