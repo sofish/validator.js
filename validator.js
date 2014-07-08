5302530da62375e21fcfe7c2ab5c61d62bf9f84c
LiangChao <liangchaoyjs@163.com>
2014-07-08 12:14:31 +0800

    添加身份证算法说明以及文档补充

--- validator.js
+++ validator.js
@@ -94,22 +94,21 @@
     },
 
     // 身份证号码验证
+    // 算法遵循 中华人民共和国国家标准 GB 11643-1999的身份证号码要求
     IDCard: function(text){
-      var isOk = (text == '');
-      if (!isOk) {
-          if ((/^\d{15}$/).test(text)) {
-              isOk = true;
-          } else if ((/^\d{17}[0-9xX]$/).test(text)) {
-              var vs = "1,0,x,9,8,7,6,5,4,3,2".split(","),
-                  ps = "7,9,10,5,8,4,2,1,6,3,7,9,10,5,8,4,2".split(","),
-                  ss = text.toLowerCase().split(""),
-                  r = 0;
-              for (var i = 0; i < 17; i++) {
-                  r += ps[i] * ss[i];
-              }
-              isOk = (vs[r % 11] == ss[17]);
+      var isOk = false;
+      if ((/^\d{15}$/).test(text)) {
+          isOk = true;
+      } else if ((/^\d{17}[0-9xX]$/).test(text)) {
+          var vs = "1,0,x,9,8,7,6,5,4,3,2".split(","),
+              ps = "7,9,10,5,8,4,2,1,6,3,7,9,10,5,8,4,2".split(","),
+              ss = text.toLowerCase().split(""),
+              r = 0;
+          for (var i = 0; i < 17; i++) {
+              r += ps[i] * ss[i];
           }
-      }     
+          isOk = (vs[r % 11] === ss[17]);
+      }   
       return isOk; 
     },    
 
